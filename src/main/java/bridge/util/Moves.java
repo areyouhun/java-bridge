6dd1695f9c8c2cc69d3d3055f2be7d71311d59a0
areyouhun <areyouhun@gmail.com>
2022-12-14 04:35:22 +0900

    refactor(moves): 클래스 safeside의 이름을 moves로 변경 및 메소드 수정

---
+++ src/main/java/bridge/util/Moves.java
@@ -0,0 +1,38 @@
+package bridge.util;
+
+import static bridge.util.Constants.ERROR_TITLE;
+import static bridge.util.Constants.MOVING_FORMAT;
+
+import java.util.Arrays;
+
+public enum Moves {
+
+    UP(1, "U"),
+    DOWN(0, "D");
+
+    private final int number;
+    private final String key;
+
+    Moves(int number, String key) {
+        this.number = number;
+        this.key = key;
+    }
+
+    public static Moves getMoveBy(String input) {
+        return Arrays.stream(Moves.values())
+                .filter(move -> move.key.equals(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ERROR_TITLE + MOVING_FORMAT));
+    }
+
+    public static String chooseUpOrDown(int number) {
+        if (number == UP.number) {
+            return UP.key;
+        }
+        return DOWN.key;
+    }
+
+    public boolean isCorrectMove(String move) {
+        return this.key.equals(move);
+    }
+}
---
+++ src/test/java/bridge/MovesTest.java
@@ -0,0 +1,30 @@
+package bridge;
+
+import static bridge.util.Constants.COLON;
+import static bridge.util.Constants.ERROR_TITLE;
+import static bridge.util.Constants.INVALID_BRIDGE_SIZE;
+import static bridge.util.Constants.MOVING_FORMAT;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import bridge.util.Moves;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MovesTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:U", "0:D"}, delimiter = COLON)
+    void 무작위값이_1이면_U를_0이면_D를_반환한다(int number, String expectedSafeSide) {
+        assertThat(Moves.chooseUpOrDown(number)).isEqualTo(expectedSafeSide);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"up", "down", "위", "아래", "u", "d"})
+    void 입력값이_대문자_U나_D가_아니면_예외를_발생시킨다(String input) {
+        assertThatThrownBy(() -> Moves.getMoveBy(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_TITLE + MOVING_FORMAT);
+    }
+}
