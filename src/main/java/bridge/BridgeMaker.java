332fe9d73dc9bea1facd31f995d23ca0d0290068
areyouhun <areyouhun@gmail.com>
2022-12-15 01:38:39 +0900

    feat(bridgemaker): 입력값에 해당하는 길이의 다리를 반환하는 메소드 생성

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,11 +1,14 @@
 package bridge;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
+
+    private static final int MINIMUM_BRIDGE_SIZE = 3;
+    private static final int MAXIMUM_BRIDGE_SIZE = 20;
+    private static final String INVALID_BRIDGE_SIZE = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -13,11 +16,24 @@ public class BridgeMaker {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
-        return null;
+        validate(size);
+        return Stream.generate(this::generateAnswerMove)
+                .limit(size)
+                .collect(Collectors.toList());
+    }
+
+    private void validate(int size) {
+        if (size < MINIMUM_BRIDGE_SIZE || size > MAXIMUM_BRIDGE_SIZE) {
+            throw new IllegalArgumentException(INVALID_BRIDGE_SIZE);
+        }
+    }
+
+    private String generateAnswerMove() {
+        final int number = bridgeNumberGenerator.generate();
+        if (number == 0) {
+            return "D";
+        }
+        return "U";
     }
 }
